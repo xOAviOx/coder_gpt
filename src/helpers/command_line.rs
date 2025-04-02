747b369067d30c4dd9294423cdfdb24c17227164
xOAviOx <avishuklacode@gmail.com>
2025-04-03 01:00:19 +0530

    Agent activity status

--- src/helpers/command_line.rs
+++ src/helpers/command_line.rs
@@ -2,7 +2,43 @@ use crossterm::{
   style::{Color, ResetColor, SetForegroundColor},
   ExecutableCommand
 };
-use std::io::{stdin,stdout};
+use std::io::{stdin,stdout, Stdout};
+
+#[derive(Debug, PartialEq)]
+
+pub enum PrintCommand {
+    AICall,
+    UnitTest,
+    Issue
+}
+
+impl PrintCommand {
+    pub fn print_agent_message(&self, agent_pos : &str, agent_statement: &str) {
+        let mut stdout: std::io::Stdout = stdout();
+
+        //decide on the print color
+
+        let statement_color:Color = match self{
+          Self::AICall => Color::Cyan,
+          Self::UnitTest => Color::Magenta,
+          Self::Issue => Color::Red,
+        };
+
+        //priont the agent statement
+
+        stdout.execute(SetForegroundColor(Color::Green)).unwrap();
+        println!("Agent: {}: ", agent_pos);
+
+
+        //make slected color
+        stdout.execute(SetForegroundColor(statement_color)).unwrap();
+        println!("{}", agent_statement);
+
+        //Reset color
+
+        stdout.execute(ResetColor).unwrap();
+    }
+}
 
 
 //get user request
@@ -30,4 +66,16 @@ pub fn get_user_response(question: &str) -> String {
   //trim whitespace and return
 
   return user_response.trim().to_string()
+}
+
+
+#[cfg(test)]
+
+mod tests {
+  use super::*;
+
+  #[test]
+  fn tests_print_agent_msg(){
+    PrintCommand::AICall.print_agent_message("Managing Agent", "Testing testing, processing something");
+  }
 }
