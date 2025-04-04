aa3b67b2984a6424e0bc62e58333ba93c92789ad
xOAviOx <avishuklacode@gmail.com>
2025-04-04 18:18:33 +0530

    Agents

--- src/apis/call_request.rs
+++ src/apis/call_request.rs
@@ -1,82 +1,99 @@
-use crate::models::general::llm::{Message,ChatCompletion, APIResponse};
+use crate::models::general::llm::{APIResponse, ChatCompletion, Message};
 use dotenv::dotenv;
 use reqwest::Client;
 use std::{env, error, f32::consts::E, sync::mpsc::SendError};
 
 use reqwest::header::{HeaderMap, HeaderValue};
 
-//call model 
+//call model
 
 pub async fn call_gpt(messages: Vec<Message>) -> Result<String, Box<dyn std::error::Error + Send>> {
-  dotenv().ok();
+    dotenv().ok();
 
-  //extract api keys
-  let api_key:String = env::var("OPEN_AI_KEY").expect("Key not found in environment variables");
-  let api_org:String = env::var("OPEN_AI_ORG").expect("Key not found in environment variables");
+    //extract api keys
+    let api_key: String = env::var("OPEN_AI_KEY").expect("Key not found in environment variables");
+    let api_org: String = env::var("OPEN_AI_ORG").expect("Key not found in environment variables");
 
+    //confirm endpoint
 
-  //confirm endpoint
+    let url: &str = "https://api.openai.com/v1/chat/completions";
 
-  let url:&str = "https://api.openai.com/v1/chat/completions";
+    //create headers
 
-  //create headers
+    let mut headers: HeaderMap = HeaderMap::new();
 
-  let mut headers: HeaderMap = HeaderMap::new();
+    //create api key header
+    headers.insert(
+        "authorization",
+        HeaderValue::from_str(&format!("Bearer {}", api_key))
+            .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?,
+    );
 
+    //create open ai org header
+    headers.insert(
+        "OpenAI-Organization",
+        HeaderValue::from_str(api_org.as_str())
+            .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?,
+    );
 
-  //create api key header
-  headers.insert("authorization", HeaderValue::from_str(&format!("Bearer {}", api_key)).map_err(|e| -> Box<dyn std::error::Error + Send> {Box::new(e)})?
-);
+    //client
 
-  //create open ai org header
-  headers.insert("OpenAI-Organization", HeaderValue::from_str(api_org.as_str()).map_err(|e| -> Box<dyn std::error::Error + Send> {Box::new(e)})?);
+    let client = Client::builder()
+        .default_headers(headers)
+        .build()
+        .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?;
 
+    //create chat completion
 
-  //client
+    let chat_completion: ChatCompletion = ChatCompletion {
+        model: "gpt-3.5-turbo".to_string(),
+        messages,
+        temperature: 0.1,
+    };
 
-  let client = Client::builder().default_headers(headers).build().map_err(|e| -> Box<dyn std::error::Error + Send> {Box::new(e)})?;
+    // //trouble shoot
 
-  //create chat completion
+    // let res_raw = client.post(url).json(&chat_completion).send().await.unwrap();
 
-  let chat_completion:ChatCompletion = ChatCompletion { model: "gpt-3.5-turbo".to_string(), messages , temperature: 0.1 };
+    // dbg!(res_raw.text().await.unwrap());
 
-  // //trouble shoot
+    // extract api res
+    let res: APIResponse = client
+        .post(url)
+        .json(&chat_completion)
+        .send()
+        .await
+        .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?
+        .json()
+        .await
+        .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?;
 
-  // let res_raw = client.post(url).json(&chat_completion).send().await.unwrap();
-
-  // dbg!(res_raw.text().await.unwrap());
-
-
-  // extract api res
-  let res:APIResponse = client.post(url).json(&chat_completion).send().await.map_err(|e| -> Box<dyn std::error::Error + Send> {Box::new(e)})?.json().await.map_err(|e| -> Box<dyn std::error::Error + Send> {Box::new(e)})?;
-
-  Ok(res.choices[0].message.content.clone())
+    Ok(res.choices[0].message.content.clone())
 }
-
 
 #[cfg(test)]
 
-mod tests{
-  use super::*;
+mod tests {
+    use super::*;
 
-  #[tokio::test]
-  async fn tests_call_to_openai(){
-    let message = Message{
-      role:"user".to_string(),
-      content: "Hi there, this is a test. Give me a very short response.".to_string()
-    };
+    #[tokio::test]
+    async fn tests_call_to_openai() {
+        let message = Message {
+            role: "user".to_string(),
+            content: "Hi there, this is a test. Give me a very short response.".to_string(),
+        };
 
-    let messages = vec![message];
+        let messages = vec![message];
 
-   let res: Result<String, Box<dyn std::error::Error + Send>>  = call_gpt(messages).await;
-    match res{
-      Ok(res_str) => {
-      dbg!(res_str);
-      assert!(true);
-    },
-    Err(_) =>{
-      assert!(false)
+        let res: Result<String, Box<dyn std::error::Error + Send>> = call_gpt(messages).await;
+        match res {
+            Ok(res_str) => {
+                dbg!(res_str);
+                assert!(true);
+            }
+            Err(_) => {
+                assert!(false)
+            }
+        }
     }
-  }
-}
 }
--- src/helpers/command_line.rs
+++ src/helpers/command_line.rs
@@ -1,34 +1,33 @@
 use crossterm::{
-  style::{Color, ResetColor, SetForegroundColor},
-  ExecutableCommand
+    style::{Color, ResetColor, SetForegroundColor},
+    ExecutableCommand,
 };
-use std::io::{stdin,stdout, Stdout};
+use std::io::{stdin, stdout, Stdout};
 
 #[derive(Debug, PartialEq)]
 
 pub enum PrintCommand {
     AICall,
     UnitTest,
-    Issue
+    Issue,
 }
 
 impl PrintCommand {
-    pub fn print_agent_message(&self, agent_pos : &str, agent_statement: &str) {
+    pub fn print_agent_message(&self, agent_pos: &str, agent_statement: &str) {
         let mut stdout: std::io::Stdout = stdout();
 
         //decide on the print color
 
-        let statement_color:Color = match self{
-          Self::AICall => Color::Cyan,
-          Self::UnitTest => Color::Magenta,
-          Self::Issue => Color::Red,
+        let statement_color: Color = match self {
+            Self::AICall => Color::Cyan,
+            Self::UnitTest => Color::Magenta,
+            Self::Issue => Color::Red,
         };
 
         //priont the agent statement
 
         stdout.execute(SetForegroundColor(Color::Green)).unwrap();
         println!("Agent: {}: ", agent_pos);
-
 
         //make slected color
         stdout.execute(SetForegroundColor(statement_color)).unwrap();
@@ -40,42 +39,43 @@ impl PrintCommand {
     }
 }
 
-
 //get user request
 pub fn get_user_response(question: &str) -> String {
-  let mut stdout: std::io::Stdout = stdout();
+    let mut stdout: std::io::Stdout = stdout();
 
-  //print the question in a specific color
+    //print the question in a specific color
 
-  stdout.execute(SetForegroundColor(Color::Blue)).unwrap();
+    stdout.execute(SetForegroundColor(Color::Blue)).unwrap();
 
-  println!(""); 
+    println!("");
 
-  println!("{}", question);
+    println!("{}", question);
 
-  //reset the color
+    //reset the color
 
-  stdout.execute(ResetColor).unwrap();
+    stdout.execute(ResetColor).unwrap();
 
-  //read user input
+    //read user input
 
-  let mut user_response: String = String::new();
+    let mut user_response: String = String::new();
 
-  stdin().read_line(&mut user_response).expect("Failed to read the response");
+    stdin()
+        .read_line(&mut user_response)
+        .expect("Failed to read the response");
 
-  //trim whitespace and return
+    //trim whitespace and return
 
-  return user_response.trim().to_string()
+    return user_response.trim().to_string();
 }
-
 
 #[cfg(test)]
 
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn tests_print_agent_msg(){
-    PrintCommand::AICall.print_agent_message("Managing Agent", "Testing testing, processing something");
-  }
+    #[test]
+    fn tests_print_agent_msg() {
+        PrintCommand::AICall
+            .print_agent_message("Managing Agent", "Testing testing, processing something");
+    }
 }
